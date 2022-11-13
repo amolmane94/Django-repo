78b1c2f0a58134dd23c7c0be6079798d59e8ce8e
root <root@ip-172-31-6-129.us-east-2.compute.internal>
2022-11-13 08:14:01 +0000

    Comment added on manage,py file

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+#this is manag.py File
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todoApp.settings')
